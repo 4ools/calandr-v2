e49865127a65a8fa1757db991c959adb81f700a4
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2020-04-20 23:25:14 +0200

    google button working mostly

--- src/components/Event/index.js
+++ src/components/Event/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import Grid from '@material-ui/core/Grid';
 import { Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import ReactPlayer from 'react-player';
+import CalendarIcon from '@material-ui/icons/EventAvailable';
 
 const useStyles = makeStyles({
   root: {
@@ -30,6 +28,16 @@ const useStyles = makeStyles({
 const EventDetails = ({ event }) => {
   const classes = useStyles();
 
+  // make a link for google calendar
+  // @TODO add meet link in there
+  let calendarUrl = 'https://calendar.google.com/calendar/render';
+  calendarUrl += '?action=TEMPLATE';
+  calendarUrl += '&dates=' + event.start;
+  calendarUrl += '/' + event.end;
+  calendarUrl += '&location=' + encodeURIComponent('at Home 💪');
+  calendarUrl += '&text=' + encodeURIComponent(event.title);
+  calendarUrl += '&details=' + encodeURIComponent(event.description);
+
   return (
     <Card className={classes.root}>
       <div className={classes.details}>
@@ -42,7 +50,12 @@ const EventDetails = ({ event }) => {
           </Typography>
         </CardContent>
         <CardActions>
-          <Button size="small" color="primary">
+          <Button
+            variant="contained"
+            color="default"
+            href={calendarUrl}
+            startIcon={<CalendarIcon />}
+          >
             Add to Google Calendar
           </Button>
         </CardActions>
