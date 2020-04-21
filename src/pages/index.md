bc24a211942abf18bef5b586c2e1bf8e2c14288e
Gareth Fuller <gareth-fuller@hotmail.co.uk>
2020-04-21 22:17:18 +0200

    added description section and meet link

--- src/pages/index.md
+++ src/pages/index.md
@@ -1,4 +1,25 @@
 ---
 templateKey: landing-page
-title: Workouts with GSUS 💪
+title: Lunch time workouts 💪
+description: >-
+  **Welcome to the lunch workout app.**
+
+
+  * Click a workout for more information  🖱
+
+  * Check out the video preview for a taste of what's to come 👀
+
+  * Add the workout to your Google Calendar to be reminded 🗓
+
+  * Click the "Go to google meet" button to join the workout! 🏋️‍♀️
+
+
+  **Please bare in mind:**
+
+
+  * Mute your 🎤 when we start as there may be many of us and it will be crazy
+
+
+  *I will not add too many workouts in the future as plans may change. However I will try keep it a few weeks in advance so there is a chance to plan. So check back often for workouts*
+meetlink: https://meet.google.com/hox-ipzp-fqf
 ---
--- src/templates/landing-page.js
+++ src/templates/landing-page.js
@@ -3,7 +3,11 @@ import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import LaunchIcon from '@material-ui/icons/Launch';
+import ReactMarkdown from 'react-markdown';
 
 import Layout from '../components/Layout';
 import Calendar from '../components/Calendar';
@@ -30,7 +34,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export const LandingPageTemplate = ({ title }) => {
+export const LandingPageTemplate = ({ title, meetlink, description }) => {
   const classes = useStyles();
 
   const [event, setEvent] = useState(null);
@@ -43,18 +47,32 @@ export const LandingPageTemplate = ({ title }) => {
     <Grid container spacing={3}>
       <Grid item xs={12}>
         <NavBar title={title} />
+        {/* https://meet.google.com/hox-ipzp-fqf */}
+      </Grid>
+      <Grid item xs={12}>
+        <Paper className={classes.paper} elevation={3}>
+          <Typography variant='h6'>
+            <ReactMarkdown source={description} />
+          </Typography>
+          <br />
+          <Button
+            variant='contained'
+            color='default'
+            href={meetlink}
+            startIcon={<LaunchIcon />}
+          >
+            Go to google meet
+          </Button>
+        </Paper>
       </Grid>
       <Grid item xs={12}>
         <Paper className={classes.calendar}>
           <Calendar selectEvent={selectEvent} />
         </Paper>
       </Grid>
-
       {event && (
         <Grid item xs={12}>
-          {/* <Paper className={classes.paper}> */}
           <Event event={event} />
-          {/* </Paper> */}
         </Grid>
       )}
       <Grid item xs={12}>
@@ -68,14 +86,20 @@ export const LandingPageTemplate = ({ title }) => {
 
 LandingPageTemplate.propTypes = {
   title: PropTypes.string,
+  description: PropTypes.string,
+  meetlink: PropTypes.string,
 };
 
 const LandingPage = ({ data }) => {
-  const { frontmatter } = data.markdownRemark;
+  const { title, description, meetlink } = data.markdownRemark.frontmatter;
 
   return (
     <Layout>
-      <LandingPageTemplate title={frontmatter.title} />
+      <LandingPageTemplate
+        title={title}
+        description={description}
+        meetlink={meetlink}
+      />
     </Layout>
   );
 };
@@ -96,6 +120,8 @@ export const pageQuery = graphql`
     markdownRemark(frontmatter: { templateKey: { eq: "landing-page" } }) {
       frontmatter {
         title
+        description
+        meetlink
       }
     }
   }
